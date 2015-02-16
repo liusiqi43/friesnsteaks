406a4dd92e56286586e1ac0733ff539f2552eb74
liusiqi43 <me@siqi.fr>
2015-02-16 19:58:49 +0100

    reclassify.py generates training set

--- data/preprocessing/reclassify.py
+++ data/preprocessing/reclassify.py
@@ -9,28 +9,29 @@ mapping = pickle.load(mapping_pkl)
 
 reclassified = {}
 for key, value in mapping.iteritems():
-	for val in value:
-		if not val in reclassified:
-			reclassified[val]=[classes[key]]
-		else:
-			reclassified[val].append(classes[key])
+    for val in value:
+        if not val in reclassified:
+            reclassified[val]=[classes[key]]
+        else:
+            reclassified[val].append(classes[key])
 
+print reclassified
 filenames = {}
 ls = listdir('../food100/output')
 for f in ls:
-	m = re.search('img_(.+?)_.*.jpg', f)
-	if m.group(1) is not None:
-		if m.group(1) in filenames:
-			filenames[int(m.group(1))].append(f)			
-		else:
-			filenames[int(m.group(1))] = [f]			
-		
-print filenames
+    m = re.search('img_(.+?)_.*.jpg', f)
+    if m is not None:
+        if int(m.group(1)) in filenames:
+            filenames[int(m.group(1))].append(f)			
+        else:
+            filenames[int(m.group(1))] = [f]			
 
-for key, value in reclassified:
-	fnames = []
-	for val in value:
-		fnames += filenames[val]
-	reclassified[key] = fnames
-	
-print reclassified
+for key, value in reclassified.iteritems():
+    fnames = []
+    for val in value:
+        fnames = fnames + filenames[val]
+    reclassified[key] = fnames
+
+
+# Use reclassified as training set for each class:
+# ex: reclassified['spaghetti'] = ['img_27_xx', ... , 'img_84_xx']
