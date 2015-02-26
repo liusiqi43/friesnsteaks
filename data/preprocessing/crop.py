4c2d53896dba2448448b4570c307c4918f3253c5
Adrien <adrienjl.rousseau@gmail.com>
2015-02-26 14:04:42 +0100

    Salut

--- data/preprocessing/crop.py
+++ data/preprocessing/crop.py
@@ -5,25 +5,18 @@ if not os.path.exists("./output/"):
 	os.makedirs("./output/")
 
 
-for root, dirs, files in os.walk("./"): #current directory
-	for name in files:
-		if name.endswith(("bb_info.txt")):
-			fo = open(os.path.join(root,name), "r+")
-			line = fo.readlines();
-			a = True;
-			#numerotation des images
-			i = 1;
-			for s in line:
-				l = s.split();
-				#On jump la premiere ligne
-				if a:
-					a = False;
-				else:
-					name1= l.pop(0)+".jpg";
-					test_image = os.path.join(root,name1);
-					original = Image.open(test_image);
-					cropped_example = original.crop((int(l.pop(0)), int(l.pop(0)), int(l.pop(0)), int(l.pop(0))))
-					if "/" in root:
-						param, value = root.split("/",1)
-					cropped_example.save("./output/"+value+"_"+str(i)+".jpg")
-					i+=1
+for item in os.listdir("./"): #explore items (including files)
+	if os.path.exists('./'+item+'/'): #if it is a folder
+		if os.path.isfile('./'+item+'/bb_info.txt'):
+			fo = open('./'+item+'/bb_info.txt', "r+")
+			lines = fo.readlines()
+			id_message = 1
+			for line in lines[1:]:
+				split = line.split()
+				name_current_image = split.pop(0)+".jpg"
+				test_image = './'+item+'/'+name_current_image
+				original = Image.open(test_image)
+				cropped_example = original.crop((int(split.pop(0)), int(split.pop(0)), int(split.pop(0)), int(split.pop(0))))
+				
+				cropped_example.save("./output/img_"+item+"_"+str(id_message)+".jpg")
+				id_message+=1
