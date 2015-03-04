b4eef0a098376cfbd4dd1a4972c82ad110a53ea0
Siqi Liu <me@siqi.fr>
2015-03-04 14:12:47 +0100

    initial commit for DDM

--- data/preprocessing/reclassify.py
+++ data/preprocessing/reclassify.py
@@ -1,41 +1,46 @@
 import pickle
 import re
-from os import listdir
+import os, sys
 from pprint import pprint
 
-classes_pkl = open('classes.pkl', 'rb')
-mapping_pkl = open('mapping.pkl', 'rb') 
-classes = pickle.load(classes_pkl)
-mapping = pickle.load(mapping_pkl)
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-reclassified = {}
-for key, value in mapping.iteritems():
-    for val in value:
-        if not val in reclassified:
-            reclassified[val]=[classes[key]]
-        else:
-            reclassified[val].append(classes[key])
+def reclassify():
+    classes_pkl = open(os.path.join(__location__, 'classes.pkl'), 'rb')
+    mapping_pkl = open(os.path.join(__location__, 'mapping.pkl'), 'rb') 
+    classes = pickle.load(classes_pkl)
+    mapping = pickle.load(mapping_pkl)
 
-filenames = {}
-ls = listdir('../food100/output')
-for f in ls:
-    m = re.search('img_(.+?)_.*.jpg', f)
-    if m is not None:
-        if int(m.group(1)) in filenames:
-            filenames[int(m.group(1))].append(f)			
-        else:
-            filenames[int(m.group(1))] = [f]			
+    reclassified = {}
+    for key, value in mapping.iteritems():
+        for val in value:
+            if not val in reclassified:
+                reclassified[val]=[classes[key]]
+            else:
+                reclassified[val].append(classes[key])
 
-example_count = {}
-for key, value in reclassified.iteritems():
-    fnames = []
-    for val in value:
-        fnames = fnames + filenames[val]
-    example_count[key] = len(fnames) 
-    reclassified[key] = fnames
+    filenames = {}
+    ls = os.listdir(os.path.join(__location__, '../food100/output_resized'))
+    for f in ls:
+        m = re.search('img_(.+?)_.*.jpg', f)
+        if m is not None:
+            if int(m.group(1)) in filenames:
+                filenames[int(m.group(1))].append(f)			
+            else:
+                filenames[int(m.group(1))] = [f]			
 
-print '{} classes, with an average of {} examples per class.'.format(len(reclassified), sum(example_count.values())/len(reclassified))
-pprint(example_count)
+    example_count = {}
+    for key, value in reclassified.iteritems():
+        fnames = []
+        for val in value:
+            fnames = fnames + filenames[val]
+        example_count[key] = len(fnames) 
+        reclassified[key] = fnames
 
-# Use reclassified as training set for each class:
-# ex: reclassified['spaghetti'] = ['img_27_xx', ... , 'img_84_xx']
+    # pprint(example_count)
+    print '{} classes, with an average of {} examples per class.'.format(len(reclassified), sum(example_count.values())/len(reclassified))
+    print 'A total of {} examples.'.format(sum(example_count.values()))
+
+    # Use reclassified as training set for each class:
+    # ex: reclassified['spaghetti'] = ['img_27_xx', ... , 'img_84_xx']
+    return reclassified, example_count
