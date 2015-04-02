74a8455e996e4a956612c960c4b594e2f9f64862
Siqi Liu <me@siqi.fr>
2015-04-02 13:28:38 +0200

    training yaml

--- datasets/food100.py
+++ datasets/food100.py
@@ -1,8 +1,3 @@
-"""
-.. todo::
-
-    WRITEME
-"""
 import os
 import logging
 
@@ -30,8 +25,8 @@ numpy.set_printoptions(threshold=numpy.nan)
 class FOOD100(dense_design_matrix.DenseDesignMatrix):
 
     def __init__(self, which_set, input_size, start=None,
-                 stop=None, axes=('b', 0, 1, 'c')):
-        self.axes = axes
+                 stop=None):
+        self.axes = ('b', 0, 1, 'c')
         self.input_size = input_size
         image_to_labels, reclassified, instance_count = reclassify()
 
@@ -39,7 +34,6 @@ class FOOD100(dense_design_matrix.DenseDesignMatrix):
         _logger.info('A total of {} examples.'.format(sum(instance_count.values())))
         ninstances = stop - start if start is not None else sum(instance_count.values())
 
-        dtype = 'uint8'
         ntrain = ninstances * .8
         ntest = ninstances * .2
         print 'ntrain = %d, ntest = %f' % (ntrain, ntest)
@@ -60,12 +54,9 @@ class FOOD100(dense_design_matrix.DenseDesignMatrix):
             string_utils.preprocess('${PYLEARN2_DATA_PATH}'),
             'food100', 'output_resized_%d' % input_size)
 
+        x = numpy.zeros((ninstances, ) + self.img_shape, dtype='float32')
         # k-hot encoding
-        x = numpy.zeros((ninstances, self.img_size), dtype=dtype)
-        y = numpy.zeros((ninstances, self.n_classes), dtype=dtype)
-
-        data = numpy.zeros((ninstances, self.img_shape[0], \
-                            self.img_shape[1], self.img_shape[2]), dtype=dtype)
+        y = numpy.zeros((ninstances, self.n_classes), dtype='int64')
 
         # load data
         i = 0
@@ -74,9 +65,12 @@ class FOOD100(dense_design_matrix.DenseDesignMatrix):
         random.shuffle(images)
         for image in images:
             img = Image.open(os.path.join(datapath, image))
-            x[i] = numpy.asarray(img).flatten('F')
+            img = numpy.asarray(img, dtype='float32')
+            # b,c,0,1
+            # img.transpose(2, 0, 1)
+            x[i] = img
             for label in image_to_labels[image]:
-                y[i][self.label_names.index(label)] = 1
+                y[i][self.label_names.index(label)] = 1.
                 break
             i = i+1
             if i == ninstances:
@@ -89,20 +83,13 @@ class FOOD100(dense_design_matrix.DenseDesignMatrix):
         Ys = {'train': y[0:ntrain],
               'test': y[ntrain:ntrain+ntest]}
 
-        X = numpy.cast['float32'](Xs[which_set])
+        X = Xs[which_set]
         y = Ys[which_set]
-
-        if isinstance(y, list):
-            y = numpy.asarray(y).astype(dtype)
 
         if which_set == 'test':
             assert X.shape[0] == ntest
 
-        view_converter = dense_design_matrix.DefaultViewConverter(self.img_shape,
-                                                                  self.axes)
-
-        super(FOOD100, self).__init__(X=X, y=y, view_converter=view_converter,
-                                      y_labels=self.n_classes)
+        super(FOOD100, self).__init__(topo_view=X, axes=self.axes, y=y, y_labels=self.n_classes)
 
         assert not contains_nan(self.X)
 
