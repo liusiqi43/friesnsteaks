473812b0c47d5fec35817a8f7c69af950e5c5765
Siqi Liu <me@siqi.fr>
2015-05-25 09:42:16 +0200

    dropout

--- experiments/train_convn_mlp.py
+++ experiments/train_convn_mlp.py
@@ -3,32 +3,37 @@ from time import gmtime, strftime
 
 # L2-regularized model
 
-yaml = open("experiments/convn_mlp.yaml", 'r').read()
+yaml = open('experiments/convn_mlp.yaml', 'r').read()
 
 hyper_params = {
-    'axes': "['b', 0, 1, 'c']",
-    "batch_size" : 25,
+    'axes': '[\'b\', 0, 1, \'c\']',
+    'batch_size' : 25,
     'input_size': 96,
-    "nb_classes" : 63,
-    "learning_rate" : 0.005,
-    "save_path" : "experiments/model_%s_live.pkl" % strftime("%Y-%m-%d_%H:%M", gmtime()),
-    "save_path_best" : "experiments/model_%s_best.pkl" % strftime("%Y-%m-%d_%H:%M", gmtime()),
+    'nb_classes' : 63,
+    'learning_rate' : 0.005,
+    'init_momentum' : 0.7,
+    'final_momentum': .95,
+    'save_path' : 'experiments/model_%s_live.pkl' % strftime('%Y-%m-%d_%H:%M', gmtime()),
+    'save_path_best' : 'experiments/model_%s_best.pkl' % strftime('%Y-%m-%d_%H:%M', gmtime()),
 
-    "output_channels_h1": 32,
-    "output_channels_h2": 64,
+    'output_channels_h1': 32,
+    'output_channels_h2': 64,
 
-    "dim_h3": 30,
+    'dim_h3': 100,
 
-    "pool_stride_h1": 2,
-    "pool_stride_h2": 2,
+    'pool_stride_h1': 2,
+    'pool_stride_h2': 2,
 
-    "pool_side_h1": 8,
-    "pool_side_h2": 8,
+    'pool_side_h1': 8,
+    'pool_side_h2': 8,
 
-    "kernel_side_h1": 5,
-    "kernel_side_h2": 5,
+    'kernel_side_h1': 5,
+    'kernel_side_h2': 5,
 
-    "sparse_init_h3": 15,
+    'max_kernel_norm_h1': 1.9,
+    'max_kernel_norm_h2': 1.9,
+    'max_col_norm_h3': 1.9365,
+    'max_col_norm_y': 1.9365
 }
 
 yaml = yaml % (hyper_params)
