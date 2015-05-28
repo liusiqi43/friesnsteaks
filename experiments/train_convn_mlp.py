42a0f993cad176ab6910a07686240fd28ff4568f
Siqi Liu <me@siqi.fr>
2015-05-28 23:36:12 +0200

    merge confusion matrix and switch to L2 regularization

--- experiments/train_convn_mlp.py
+++ experiments/train_convn_mlp.py
@@ -10,40 +10,40 @@ model_desc = '# Dropout model with max_norm constraints\n'
 
 hyper_params = {
     'axes': '[\'b\', 0, 1, \'c\']',
-    'batch_size' : 25,
-    'input_size': 96,
+    'batch_size' : 50,
+    'input_size': 36,
     'nb_classes' : 63,
 
-    'learning_rate' : 0.01,
-    'lr_decay_factor': 0.1,
+    'learning_rate' : .01,
+    'lr_decay_factor': .1,
 
-    'init_momentum' : 0.7,
-    'final_momentum': .99,
+    'init_momentum' : .5,
+    'final_momentum': .7,
     'save_path' : os.path.join(__location__, 'model_%s_live.pkl' % model),
     'save_path_best' : os.path.join(__location__, 'model_%s_best.pkl' % model),
     'output_channels_h1': 32,
     'output_channels_h2': 64,
 
-    'dim_h3': 60,
-    'dim_h4': 60,
+    'dim_h3': 200,
+    'dim_h4': 200,
 
     'pool_stride_h1': 2,
     'pool_stride_h2': 2,
 
-    'pool_side_h1': 5,
-    'pool_side_h2': 5,
+    'pool_side_h1': 4,
+    'pool_side_h2': 4,
 
     'kernel_side_h1': 8,
     'kernel_side_h2': 8,
 
-    'max_kernel_norm_h1': 2.5,
-    'max_kernel_norm_h2': 2.5,
-    'max_col_norm_h3': 2.5,
-    'max_col_norm_h4': 2.5,
+    'max_kernel_norm_h1': 1.9365,
+    'max_kernel_norm_h2': 1.9365,
+    'max_col_norm_h3': 1.9365,
+    'max_col_norm_h4': 1.9365,
     'max_col_norm_y': 1.9365,
 
-    'irange': .1,
-    'istdev': .1,
+    'irange': .05,
+    'istdev': .05,
     'sparse_init': 15,
 }
 
