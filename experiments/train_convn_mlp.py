29318d4a5d5a0aae9355d3b51251890a6ab55430
Siqi Liu <me@siqi.fr>
2015-05-29 12:49:17 +0200

    architecture inspired by stanford cs231n guide

--- experiments/train_convn_mlp.py
+++ experiments/train_convn_mlp.py
@@ -6,12 +6,12 @@ __location__ = os.path.dirname(os.path.abspath(__file__))
 yaml = open(os.path.join(__location__, 'convn_mlp.yaml'), 'r').read()
 model = strftime('%Y-%m-%d_%H:%M', gmtime())
 
-model_desc = '# Dropout model with max_norm constraints\n'
+model_desc = '#Architecture inspired by stanford guide\n'
 
 hyper_params = {
     'axes': '[\'b\', 0, 1, \'c\']',
     'batch_size' : 50,
-    'input_size': 36,
+    'input_size': 64,
     'nb_classes' : 63,
 
     'learning_rate' : .01,
@@ -21,26 +21,21 @@ hyper_params = {
     'final_momentum': .7,
     'save_path' : os.path.join(__location__, 'model_%s_live.pkl' % model),
     'save_path_best' : os.path.join(__location__, 'model_%s_best.pkl' % model),
+
+    'output_channels_h0': 32,
     'output_channels_h1': 32,
     'output_channels_h2': 64,
+    'output_channels_h3': 64,
+    'output_channels_h4': 256,
+    'output_channels_h5': 256,
 
-    'dim_h3': 200,
-    'dim_h4': 200,
+    'kernel_side_h0': 7,
+    'kernel_side_conv': 3,
 
-    'pool_stride_h1': 2,
-    'pool_stride_h2': 2,
+    'pool_stride_conv': 2,
+    'pool_side_conv': 2,
 
-    'pool_side_h1': 4,
-    'pool_side_h2': 4,
-
-    'kernel_side_h1': 8,
-    'kernel_side_h2': 8,
-
-    'max_kernel_norm_h1': 1.9365,
-    'max_kernel_norm_h2': 1.9365,
-    'max_col_norm_h3': 1.9365,
-    'max_col_norm_h4': 1.9365,
-    'max_col_norm_y': 1.9365,
+    'max_norm': 1.9365,
 
     'irange': .05,
     'istdev': .05,
