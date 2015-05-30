0165f76e04fc251c350f97dbac5614a7eead49ac
Siqi Liu <me@siqi.fr>
2015-05-30 11:38:11 +0200

    applying dropout to all layers

--- experiments/train_convn_mlp.py
+++ experiments/train_convn_mlp.py
@@ -6,7 +6,7 @@ __location__ = os.path.dirname(os.path.abspath(__file__))
 yaml = open(os.path.join(__location__, 'convn_mlp.yaml'), 'r').read()
 model = strftime('%Y-%m-%d_%H:%M', gmtime())
 
-model_desc = '#Architecture inspired by stanford guide\n'
+model_desc = '#Architecture inspired by stanford guide with dropout\n'
 
 hyper_params = {
     'axes': '[\'b\', 0, 1, \'c\']',
@@ -14,22 +14,22 @@ hyper_params = {
     'input_size': 64,
     'nb_classes' : 63,
 
-    'learning_rate' : .01,
+    'learning_rate' : .1,
     'lr_decay_factor': .1,
 
-    'init_momentum' : .5,
-    'final_momentum': .7,
+    'init_momentum' : .8,
+    'final_momentum': .99,
     'save_path' : os.path.join(__location__, 'model_%s_live.pkl' % model),
     'save_path_best' : os.path.join(__location__, 'model_%s_best.pkl' % model),
 
-    'output_channels_h0': 32,
-    'output_channels_h1': 32,
-    'output_channels_h2': 64,
-    'output_channels_h3': 64,
+    'output_channels_h0': 128,
+    'output_channels_h1': 128,
+    'output_channels_h2': 128,
+    'output_channels_h3': 128,
     'output_channels_h4': 128,
     'output_channels_h5': 128,
-    'output_channels_h6': 256,
-    'output_channels_h7': 256,
+    'output_channels_h6': 128,
+    'output_channels_h7': 128,
 
     'kernel_side_h0': 5,
     'kernel_side_conv': 3,
