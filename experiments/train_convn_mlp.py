4978e25a112ca227abfc358462016e73558c10f4
Siqi Liu <me@siqi.fr>
2015-06-01 14:14:53 +0200

    long running

--- experiments/train_convn_mlp.py
+++ experiments/train_convn_mlp.py
@@ -10,38 +10,36 @@ model_desc = '#Architecture inspired by stanford guide with dropout\n'
 
 hyper_params = {
     'axes': '[\'b\', 0, 1, \'c\']',
-    'batch_size' : 25,
+    'batch_size' : 32,
     'input_size': 64,
     'nb_classes' : 63,
 
-    'learning_rate' : .1,
-    'lr_decay_factor': .1,
+    'learning_rate' : .04,
+    'lr_decay_factor': .05,
 
-    'init_momentum' : .8,
+    'init_momentum' : .7,
     'final_momentum': .99,
     'save_path' : os.path.join(__location__, 'model_%s_live.pkl' % model),
     'save_path_best' : os.path.join(__location__, 'model_%s_best.pkl' % model),
 
-    'output_channels_h0': 32,
-    'output_channels_h1': 32,
-    'output_channels_h2': 64,
-    'output_channels_h3': 64,
-    'output_channels_h4': 128,
-    'output_channels_h5': 128,
-    'output_channels_h6': 256,
-    'output_channels_h7': 256,
+    'output_channels_h0': 64,
+    'output_channels_h1': 64,
+    'output_channels_h2': 128,
+    'output_channels_h3': 128,
+    'output_channels_h4': 256,
+    'output_channels_h5': 256,
+    'output_channels_h6': 512,
+    'output_channels_h7': 512,
 
-    'kernel_side_h0': 5,
-    'kernel_side_conv': 3,
+    'kernel_side_h0': 7,
+    'kernel_side_conv': 5,
 
     'pool_side_conv': 3,
     'pool_stride_conv': 2,
 
     'max_norm': 1.9365,
 
-    'irange': .3,
-
-    'weight_decay': .01,
+    'irange': .03,
 }
 
 yaml = yaml % (hyper_params)
